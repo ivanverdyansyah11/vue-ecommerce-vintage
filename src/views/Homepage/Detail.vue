ed6d97d2128b4d6a9e4472946453f4cffd801226
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-23 06:43:20 +0800

    Progress: Update Mainpage Detail View

---
+++ src/views/Homepage/Detail.vue
@@ -0,0 +1,101 @@
+<template>
+    <!-- MAIN -->
+    <main class="container section-gap">
+        <section class="collection-detail">
+            <div class="row justify-content-center ">
+                <div class="col-lg-8 col-xl-6 mb-4 mb-lg-0">
+                    <div class="row row-cols-2 row-cols-md-3 g-2 ">
+                        <div class="col">
+                            <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/1592a4e8-fc66-492a-97fe-5b84a36e68c1/sportswear-tech-fleece-windrunner-hoodie-hXT8wQ.png" alt="Product Detail Image" class="img-fluid" style="width: 100%; height: 100%; object-fit: cover;">
+                        </div>
+                        <div class="col">
+                            <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9c3ceefd-d60c-4fcc-b668-ba4272fdbcb4/sportswear-tech-fleece-windrunner-hoodie-hXT8wQ.png" alt="Product Detail Image" class="mb-2" style="width: 100%; height: 200px; object-fit: cover;">
+                            <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8c8c5787-bb35-4254-b246-c0bfdd80b664/sportswear-tech-fleece-windrunner-hoodie-hXT8wQ.png" alt="Product Detail Image" style="width: 100%; height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="col col-last">
+                            <div class="row g-2">
+                                <div class="col-6 col-md-12">
+                                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/acc63859-adab-4501-a911-45c75791d2cc/sportswear-tech-fleece-windrunner-hoodie-hXT8wQ.png" alt="Product Detail Image" class="mb-2" style="width: 100%; height: 200px; object-fit: cover;">
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3e810ec7-f562-4fe4-bed8-38f5e632ef00/sportswear-tech-fleece-windrunner-hoodie-hXT8wQ.png" alt="Product Detail Image" style="width: 100%; height: 200px; object-fit: cover;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-xxl-3">
+                    <div class="card-detail">
+                        <div class="wrapper d-flex justify-content-between gap-2">
+                            <div class="wrapper-product">
+                                <h4 class="detail-price">Rp 1,799,000</h4>
+                                <p class="detail-name">Nike Sportswear Tech Fleece Windrunner</p>
+                                <ul>
+                                    <li>8 / M</li>
+                                    <li>Very Good</li>
+                                    <li>Denpasar</li>
+                                </ul>
+                            </div>
+                            <i class="fa-regular fa-heart"></i>
+                        </div>
+                        <p class="detail-title">Item Description</p>
+                        <p class="detail-description">Blending 2 of our most iconic looks, this full-zip hoodie draws design inspiration from our timeless Windrunner jacket, as well as our Tech Fleece jacket. It's designed to feel relaxed through the shoulders, chest and body for an athletic fit you can layer. Our premium, smooth-on-both-sides fleece feels warmer and softer than ever while keeping the same lightweight build you love.</p>
+                        <div class="row row-cols-2 detail-product">
+                            <div class="col mb-2">
+                                <p>Category</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>: Hoodies</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>Size</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>: 8/M</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>Condition</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>: Very Good</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>Color</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>: White</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>Uploaded</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>: 5 hours ago</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>Shipping</p>
+                            </div>
+                            <div class="col mb-2">
+                                <p>: Rp. 599.000</p>
+                            </div>
+                        </div>
+                        <div class="button-group d-flex flex-column gap-2">
+                            <button class="button-primary-small">Buy Now</button>
+                            <button class="button-reverse-small">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- END MAIN -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
