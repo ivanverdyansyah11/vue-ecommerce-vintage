1e460ec7bb6d939958967bb51f4486b358ac6e60
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-23 06:42:21 +0800

    Progress: Update Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import MainPage from '../views/MainPage.vue'
-// import DetailProduct from '../views/DetailProduct.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'mainpage',
-      component: () => import('../views/MainPage.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/auth/Login.vue'),
     },
     {
-      path: '/detail',
-      name: 'detail',
-      component: () => import('../views/DetailProduct.vue'),
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/auth/Signup.vue'),
+    },
+    {
+      path: '/',
+      redirect:'/mainpage',
+      component: () => import('../views/layout/Homepage.vue'),
+      children: [
+        {
+          path: '/mainpage',
+          name: 'mainpage',
+          component: () => import('../views/Homepage/Mainpage.vue'),
+        },
+        {
+          path: '/mainpage/products',
+          name: 'products',
+          component: () => import('../views/Homepage/Products.vue'),
+        },
+        {
+          path: '/mainpage/detail',
+          name: 'detail',
+          component: () => import('../views/Homepage/Detail.vue'),
+        },
+        {
+          path: '/profile',
+          name: 'profile',
+          component: () => import('../views/Homepage/Profile.vue'),
+        },
+      ]
     },
   ]
 })
