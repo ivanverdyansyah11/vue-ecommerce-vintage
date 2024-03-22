b32adffaf236064ecf5004ae67c1536e1a191851
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-23 06:42:30 +0800

    Progress: Update Component Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,53 +1,56 @@
 <template>
   <!-- FOOTER -->
-    <footer class="footer container" id="footer">
-        <div class="footer-profile">
-            <img src="/src/assets/images/brand/logo-brand-light.svg" alt="Brand Logo" height="26">
-            <p class="footer-caption">At Oreon, fashion is not just about clothing; it's a lifestyle. We believe in empowering individuals to express themselves through style.</p>
-            <div class="footer-sosmed">
-                <a target="_blank" href="https://www.facebook.com/" class="sosmed-link">
-                    <img src="/src/assets/images/icon/facebook.svg" alt="Facebook Icon" width="16">
-                </a>
-                <a target="_blank" href="whatsapp://send?text=Hello&phone=+6281238484005" class="sosmed-link">
-                    <img src="/src/assets/images/icon/whatsapp.svg" alt="Whatsapp Icon" width="16">
-                </a>
-                <a target="_blank" href="https://twitter.com/?lang=id" class="sosmed-link">
-                    <img src="/src/assets/images/icon/twitter.svg" alt="Twitter Icon" width="16">
-                </a>
+    <footer class="footer section-gap">
+        <div class="container">
+            <div class="row row-cols-2 row-cols-md-4 footer-link">
+                <div class="col mb-5 mb-md-0">
+                    <h6 class="footer-title">Vintage</h6>
+                    <ul>
+                        <li><a href="#about">About Us</a></li>
+                        <li><a href="#sustainability">Sustainability</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#advertising">Advertising</a></li>
+                    </ul>
+                </div>
+                <div class="col mb-5 mb-md-0">
+                    <h6 class="footer-title">Discover</h6>
+                    <ul>
+                        <li><a href="#howitwork">How it Work</a></li>
+                        <li><a href="#helpcenter">Help Center</a></li>
+                        <li><a href="#infoboard">Infoboard</a></li>
+                        <li><a href="#mobileapps">Mobile apps</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h6 class="footer-title">Help</h6>
+                    <ul>
+                        <li><a href="#helpcenter">Help center</a></li>
+                        <li><a href="#buying">Buying</a></li>
+                        <li><a href="#trustsafety">Trust & Safety</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h6 class="footer-title">Community</h6>
+                    <ul>
+                        <li><a href="#forum">Forum</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row footer-copyright">
+                <div class="col-12 d-flex align-items-center justify-content-between ">
+                    <div class="wrapper">
+                        <ul>
+                            <li><a target="_blank" href="https://wa.me/+6281238484005"><i class="fa-brands fa-whatsapp"></i></a></li>
+                            <li><a target="_blank" href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
+                            <li><a target="_blank" href="https://id.linkedin.com/"><i class="fa-brands fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                    <p>© Vintage 2023</p>
+                </div>
             </div>
         </div>
-        <div class="footer-link">
-            <h6 class="footer-title">Quick Link</h6>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#products">Trending Product</a></li>
-                <li><a href="#about">About Us</a></li>
-                <li><a href="#gallery">Documentation Product</a></li>
-            </ul>
-        </div>
-        <div class="footer-link">
-            <h6 class="footer-title">Category Product</h6>
-            <ul>
-                <li><a href="#">New Arrivals</a></li>
-                <li><a href="#">Shoes</a></li>
-                <li><a href="#">Clothing</a></li>
-                <li><a href="#">Accessories</a></li>
-            </ul>
-        </div>
-        <div class="footer-link">
-            <h6 class="footer-title">Help & Guide</h6>
-            <ul>
-                <li><a href="#">Terms & Conditions</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-        </div>
     </footer>
-
-    <footer class="copyright">
-        <p class="label">Copyright © 2024 OREON. All Right Reserved.</p>
-    </footer>
-    <!-- END FOOTER -->    
+    <!-- END FOOTER -->  
 </template>
 
 <script>
